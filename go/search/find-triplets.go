c92283a4debb46f53c22f10e3ab205eb365f3cdf
Samanta <Arajit.Samanta@ehi.com>
2018-09-27 15:02:44 -0500

    restructure code

--- go/search/find-triplets.go
+++ go/search/find-triplets.go
@@ -5,7 +5,6 @@ import (
 	"sort"
 )
 
-//Remove duplicate from an array
 func removeDuplicate(arr []int32) []int32 {
 	keys := make(map[int32]bool)
 	list := []int32{}
@@ -52,91 +51,45 @@ func sortIt(arr []int32) {
 	})
 }
 
-func binarySearch(arr []int32, n int, toSearch int32, flagLeftSearch bool) int {
-	var (
-		mid   int
-		found bool
-	)
-	start := 0
-	end := n - 1
-	//n = 3
-
-	for start <= end {
-
-		mid = (start + end) / 2
-
-		if arr[mid] == toSearch {
-			found = true
-			break
-		}
-
-		if arr[mid] > toSearch {
-			end = mid - 1
-		} else {
-			start = mid + 1
-		}
-
-	}
-
-	fmt.Println(mid, arr[mid])
-
-	/*if flagLeftSearch {
-		if toSearch <= arr[mid] {
-			return (n - mid + 1)
-		}
-	} else {
-		if toSearch >= arr[mid] {
-			return (n - mid + 1)
-		}
-	}*/
-
-	if found {
-		return mid + 1
-	}
-
-	if flagLeftSearch {
-		if toSearch <= arr[mid] {
-			return (n - mid + 1)
-		}
-	} else {
-		if toSearch >= arr[mid] {
-			return (n - mid + 1)
-		}
-	}
-
-	return 0
-}
-
 func tripletsSmart(a []int32, b []int32, c []int32) int64 {
 	var (
-		count int64
+		count                        int64
+		idxA, idxB, idxC, aDup, cDup int
+		q                            int32
 	)
-
-	//Remove duplicates
-	a = removeDuplicate(a)
-	b = removeDuplicate(b)
-	c = removeDuplicate(c)
 
 	//Sort the arrays
 	sortIt(a)
 	sortIt(b)
 	sortIt(c)
-	fmt.Println(a, b, c)
 
 	lenA := len(a)
 	lenB := len(b)
 	lenC := len(c)
 
 	//complexity is O(n^3)
-	for i := 0; i < lenB; i++ {
-		q := b[i]
+	for idxB = 0; idxB < lenB; idxB++ {
+		q = b[idxB]
+		for idxA < lenA && a[idxA] <= q {
+			// count duplicates in A array starting at index 1
+			if idxA > 0 && a[idxA] == a[idxA-1] {
+				aDup++
+			}
+			idxA++
+		}
 
-		lCount := binarySearch(a, lenA, q, true)
-		fmt.Println("left:", lCount)
+		for idxC < lenC && c[idxC] <= q {
+			// count duplicates in C array starting at index 1
+			if idxC > 0 && c[idxC] == c[idxC-1] {
+				cDup++
+			}
+			idxC++
+		}
 
-		rCount := binarySearch(c, lenC, q, false)
-		fmt.Println("right", rCount)
-		count += int64(lCount * rCount)
+		// Ignore duplicates in 'b' array; make sure terms are int64!
+		if idxB == 0 || b[idxB-1] != b[idxB] {
+			count += int64((idxA - aDup)) * int64((idxC - cDup))
+		}
 
 	}
 
--- go/search/find-triplets_test.go
+++ go/search/find-triplets_test.go
@@ -12,8 +12,9 @@ func TestFindPossibleTriplets(t *testing.T) {
 		want int64
 	}{
 		{[]int32{1, 3, 5}, []int32{2, 3}, []int32{1, 2, 3}, 8},
-		//{[]int32{3, 5, 7}, []int32{3, 6}, []int32{4, 6, 9}, 4},
-		/*{[]int32{5, 4, 1}, []int32{2, 3, 3}, []int32{3, 2, 1}, 5},*/
+		{[]int32{3, 5, 7}, []int32{3, 6}, []int32{4, 6, 9}, 4},
+		{[]int32{5, 4, 1}, []int32{2, 3, 3}, []int32{3, 2, 1}, 5},
+		{[]int32{1, 4, 5, 7}, []int32{5, 7, 9}, []int32{7, 9, 11, 13}, 12},
 	}
 	for _, c := range cases {
 		got := tripletsSmart(c.a, c.b, c.c)
